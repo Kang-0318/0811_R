281fcacbe071d26d451b194e35f73d02b0e0eaa8
Kang-0318 <audiogarden2@kbu.ac.kr>
2025-08-11 16:55:59 +0900

    a

---
+++ src/pages/Board.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Board =() => {
+    const dummy = [
+        {id:1,title:"React Router 시작하기"},
+        {id:2,title:"URL 파라미터 이해하기"},
+        {id:3,title:"NavLink와 활성 스타일"},
+    ];
+    return (
+        <section>
+            <h1>게시글</h1>
+            <ul>
+                
+            </ul>
+        </section>
+    )
+
+}
